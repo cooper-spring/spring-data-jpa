f024d941e7067821d92e36c20fe63b05afd996df
cooper <pbg0205@naver.com>
2022-05-15 21:20:16 +0900

    jpa-practice01 first commit

---
+++ jpa-practice01/src/test/java/com/cooper/jpapractice01/JpaPractice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.jpapractice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPractice01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
