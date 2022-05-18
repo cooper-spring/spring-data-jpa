d2feeb2de190b617679becd332dd0de6c352d2ed
박병기 <bgpark@rsupport.com>
2022-05-18 17:45:49 +0900

    eneity 관리 테스트를 위한 프로젝트 레포지토리 생성

---
+++ entity-management/src/test/java/com/example/entitymanagement/EntityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
