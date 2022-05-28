5a68df9e6d0c6a565403b7da7943ba287f99bf68
cooper <pbg0205@naver.com>
2022-05-28 13:00:24 +0900

    spring-data-jpa & realmysql practice

---
+++ jpa-real-mysql/src/main/java/com/cooper/jparealmysql/JpaRealMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.jparealmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRealMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRealMysqlApplication.class, args);
+    }
+
+}
---
+++ jpa-real-mysql/src/test/java/com/cooper/jparealmysql/JpaRealMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.jparealmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRealMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
